e27645ca2f8acfffda28b1b8e81748ceb56fe3a1
narcisacodreanu <narcisacodreanu@users.noreply.github.com>
2018-04-10 23:11:32 -0400

    finished hw 2

--- redis/homework_2.py
+++ redis/homework_2.py
@@ -16,6 +16,16 @@ def article_vote(redis, user, article):
 
 def article_switch_vote(redis, user, from_article, to_article):
     # HOMEWORK 2 Part I
+    
+	# decrement vote by adding the negative of the vote score
+    redis.zincrby(name='score:', value=from_article, amount=-VOTE_SCORE)
+    redis.hincrby(name=from_article, key='votes', amount=-1)
+
+    # add vote to other article
+    article_id = to_article.split(':')[-1]
+    if redis.sadd('voted:' + article_id, user):
+        redis.zincrby(name='score:', value=to_article, amount=VOTE_SCORE)
+        redis.hincrby(name=to_article, key='votes', amount=1)
     pass
 
 redis = redis.StrictRedis(host='localhost', port=6379, db=0)
@@ -29,6 +39,9 @@ article_switch_vote(redis, "user:2", "article:8", "article:1")
 # Which article's score is between 10 and 20?
 # PRINT THE ARTICLE'S LINK TO STDOUT:
 # HOMEWORK 2 Part II
-# article = redis.?
-# print redis.?
+
+# get the id of the article
+article = redis.zrangebyscore('score:', 10, 20).pop(0)
+# get the str for the article
+print (redis.hget(name=article, key='link').decode('UTF-8'))
 
